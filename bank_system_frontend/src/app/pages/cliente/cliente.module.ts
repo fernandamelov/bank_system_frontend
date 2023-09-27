4a9cc2fae5ebaeb810e879b1ade0581b0dfa8e96
Fernanda Melo <fernanda.melo@servicenet.com.br>
2023-09-26 23:24:59 -0300

    sistema bancario para front end angular e node. html, scss e ts

---
+++ bank_system_frontend/src/app/pages/cliente/cliente.module.ts
@@ -0,0 +1,39 @@
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RouterModule } from '@angular/router';
+import { MaterialModule } from 'src/app/shared/material/material.module';
+import { HttpClientModule } from '@angular/common/http';
+import { ReactiveFormsModule } from '@angular/forms';
+import { NgxMaskDirective, NgxMaskPipe, provideNgxMask } from 'ngx-mask';
+import { CadastroClienteComponent } from './cadastro-cliente/cadastro-cliente.component';
+import { ListagemClienteComponent } from './listagem-cliente/listagem-cliente.component';
+
+
+@NgModule({
+  declarations: [
+    CadastroClienteComponent,
+    ListagemClienteComponent
+  ],
+  imports: [
+    CommonModule,
+    FormsModule,
+    BrowserAnimationsModule,
+    ReactiveFormsModule,
+    RouterModule,
+    HttpClientModule,
+    MaterialModule,
+    NgxMaskDirective,
+    NgxMaskPipe
+   ],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
+  providers: [provideNgxMask()],
+  exports: [
+    ListagemClienteComponent,
+    CadastroClienteComponent,
+    NgxMaskDirective, 
+    NgxMaskPipe,  
+  ],
+})
+export class ClienteModule { }
